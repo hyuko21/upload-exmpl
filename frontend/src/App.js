6dcf9e21e897ca8ee7beb95e74c293cb85375434
victor <victorwdr1@gmail.com>
2019-02-01 10:32:56 -0300

    implemented file upload to api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,85 @@
 import React, { Component } from "react";
+import uniqueId from "lodash/uniqueId";
+import filesize from "file-size";
+
+import api from "./services/api";
 
 import GlobalStyle from "./styles/global";
 import { Container, Content } from "./styles";
 
 import Upload from "./components/Upload";
+import FileList from "./components/FileList";
 
 class App extends Component {
+  state = {
+    uploadedFiles: []
+  };
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null
+    }));
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
+
+    uploadedFiles.forEach(this.processUpload);
+  };
+
+  updateFile = (id, data) => {
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+        return id === uploadedFile.id
+          ? { ...uploadedFile, ...data }
+          : uploadedFile;
+      })
+    });
+  };
+
+  processUpload = ({ id, file, name }) => {
+    const data = new FormData();
+
+    data.append("file", file, name);
+
+    api
+      .post("posts", data, {
+        onUploadProgress: e => {
+          const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+
+          this.updateFile(id, { progress });
+        }
+      })
+      .then(({ data }) => {
+        this.updateFile(id, {
+          uploaded: true,
+          id: data._id,
+          url: data.url
+        });
+      })
+      .catch(err => {
+        this.updateFile(id, {
+          error: true
+        });
+      });
+  };
+
   render() {
+    const { uploadedFiles } = this.state;
+
     return (
       <Container>
         <Content>
-          <Upload />
+          <Upload onUpload={this.handleUpload} />
+          {!!uploadedFiles.length && <FileList files={uploadedFiles} />}
         </Content>
         <GlobalStyle />
       </Container>
---
+++ frontend/src/components/FileList/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import CircularProgressbar from "react-circular-progressbar";
+import { MdCheckCircle, MdError, MdLink } from "react-icons/md";
+import { Container, FileInfo, Preview } from "./styles";
+
+const truncate = (string, length) => {
+  const truncatedString = `${string.substr(0, length - 3)}...`;
+
+  return (string.length > truncatedString.length && truncatedString) || string;
+};
+
+const renderFileInfo = ({
+  id,
+  name,
+  readableSize,
+  preview,
+  progress,
+  uploaded,
+  error,
+  url
+}) => (
+  <li key={id}>
+    <FileInfo>
+      <Preview src={preview} />
+      <div>
+        <strong>{truncate(name, 28)}</strong>
+        <span>
+          {readableSize.human()}{" "}
+          {!!url && <button onClick={() => {}}>Excluir</button>}
+        </span>
+      </div>
+    </FileInfo>
+    <div>
+      {!uploaded && !error && (
+        <CircularProgressbar
+          styles={{
+            root: { width: 24 },
+            path: { stroke: "#7159c1" }
+          }}
+          strokeWidth={10}
+          percentage={progress}
+        />
+      )}
+      {url && (
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
+        </a>
+      )}
+      {uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+      {error && <MdError size={24} color="#e57878" />}
+    </div>
+  </li>
+);
+
+const FileList = ({ files }) => (
+  <Container>{files.map(renderFileInfo)}</Container>
+);
+
+export default FileList;

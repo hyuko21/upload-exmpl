7d0ccea498153efa4c17866bbde1b8381c2eee4a
victor <victorwdr1@gmail.com>
2019-02-01 10:32:17 -0300

    added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,17 +4,16 @@ const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
 /**
  * DATABASE SETUP
  */
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
